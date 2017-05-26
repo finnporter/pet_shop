a89ff769e9fb345ec081ec5654f2764be0013163
finnporter <iamfinnporter@gmail.com>
2017-05-26 20:48:35 +0100

    test_remove_pet_by_name

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,18 @@ end
 def stock_count(shop)
   return shop[:pets].length
 end
+
+def pets_by_breed(shop, breed)
+  pets = shop[:pets].select { |pet| pet[:breed] == breed}
+  return pets
+end
+
+def find_pet_by_name(pet_shop, pet_name)
+  pets = pet_shop[:pets].find { |pet| pet[:name] == pet_name}
+  return pets
+end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  pet_to_be_removed = find_pet_by_name(pet_shop, pet_name)
+  pet_shop[:pets].delete(pet_to_be_removed)
+end
